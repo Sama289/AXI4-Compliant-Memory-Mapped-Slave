6bfa2683bc2cd475dbb09aec811e0abd5e89b80f
Sama Ashraf <samaashraf2892002@gmail.com>
2026-03-11 08:45:39 +0200

    Remove coverage report folder from repository

--- cov_report/files/instlink.js
+++
@@ -1,6 +0,0 @@
-<<<<<<< HEAD
-var g_data = {"11":[10,"axi4if",1],"14":[12,"axi4_sva_inst",1],"12":[10,"uut",1],"15":[10,"tb",1],"10":[-1,"axi4_top",1],"17":[-1,"axi_packet_pkg",1]};
-=======
-var g_data = {"11":[10,"axi4if",1],"14":[12,"axi4_sva_inst",1],"12":[10,"uut",1],"15":[10,"tb",1],"10":[-1,"axi4_top",1],"17":[-1,"axi_packet_pkg",1]};
->>>>>>> 7968f542d993b7369b31cb2b1d005d0ea04dc4bd
-processInstLinks(g_data);
